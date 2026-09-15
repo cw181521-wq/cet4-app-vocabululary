53a6f57b0ef0d4561c41ef7aad5427c3212b1adc
cw181521-wq <cw181521@gmail.com>
2026-09-15 15:26:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // MorphoSyntax Pro - Service Worker (PWA Offline Engine)
-const CACHE_NAME = 'morpho-pwa-v1.0.4';
+const CACHE_NAME = 'morpho-pwa-v1.0.5';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
