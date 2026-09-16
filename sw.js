dacd7bc9a45b8061e25b59f9e655ce5d22c9a2ba
cw181521-wq <cw181521@gmail.com>
2026-09-16 19:53:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,14 @@
-const CACHE_NAME = 'morphosyntax-v3.2-0916c';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'morpho-pwa-v2.0';
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './words_data.js'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
@@ -17,9 +16,7 @@ self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.map((k) => {
-          if (k !== CACHE_NAME) return caches.delete(k);
-        })
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
       );
     }).then(() => self.clients.claim())
   );
@@ -31,9 +28,6 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then((cached) => {
       if (cached) return cached;
       return fetch(e.request).then((resp) => {
-        if (!resp || resp.status !== 200 || resp.type !== 'basic') return resp;
-        const copy = resp.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, copy));
         return resp;
       }).catch(() => caches.match('./index.html'));
     })
